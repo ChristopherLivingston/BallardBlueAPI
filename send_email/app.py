138fc0a47333902bc66f0398f6dde2d0568438d8
ChristopherLivingston <BallardBlueLLC@gmail.com>
2024-11-11 14:10:13 -0800

    Updating response headers to address CORS issue

--- send_email/app.py
+++ send_email/app.py
@@ -61,6 +61,11 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
+            'headers': {
+                         "Access-Control-Allow-Origin": "*",
+                         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                         "Access-Control-Allow-Headers": "Content-Type, X-Amz-Date, Authorization, X-Api-Key",
+            },
             'body': json.dumps({
                 'message': 'Email sent successfully!',
                 'message_id': response['MessageId']
