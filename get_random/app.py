253cec0f6a745f44a4b9e05f2f01b69c83ffa0d5
ChristopherLivingston <BallardBlueLLC@gmail.com>
2024-11-12 12:45:22 -0800

    Get Random API Checkpoint 2

--- get_random/app.py
+++ get_random/app.py
@@ -1,10 +1,43 @@
 import json
 import boto3
+import random
+
+from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
+
+
+def get_random():
+    try:
+        dynamodb = boto3.client('dynamodb', region_name='us-west-2')
+        response = dynamodb.scan(TableName='magic_inventory')
+        items = response.get('Items', [])
+        if items[0]:
+            return {
+                'card_name': items[0]['card_name']['S'],
+                'set_name': items[0]['set_name']['S']
+            }
+        else:
+            raise ValueError("No items found in the table.")
+
+    except ClientError as e:
+        # Handle AWS service related errors
+        print(f"An error occurred with DynamoDB: {e}")
+        return None
+
+    except ValueError as e:
+        # Handle specific ValueError if no items are found
+        print(f"Error: {e}")
+        return None
+
+    except Exception as e:
+        # Handle any other unexpected errors
+        print(f"An unexpected error occurred: {e}")
+        return None
 
 
 def lambda_handler(event, context):
     try:
+        random_card = get_random()
         return {
             'statusCode': 200,
             'headers': {
@@ -13,7 +46,7 @@ def lambda_handler(event, context):
                          "Access-Control-Allow-Headers": "Content-Type, X-Amz-Date, Authorization, X-Api-Key",
             },
             'body': json.dumps({
-                'message': 'Here is a random card!'
+                'message': random_card
             })
         }
     except ClientError as e:
